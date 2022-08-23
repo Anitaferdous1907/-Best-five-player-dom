373bda884bb6db70b4c1da35a691b654ef078c9f
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-08-23 21:30:31 +0600

    player expense part done

--- js/select-best-five.js
+++ js/select-best-five.js
@@ -1,16 +1,48 @@
+
+document.getElementById('btn-field').addEventListener('click', function () {
+
+    const amountField = document.getElementById('amount-field');
+    const newPlayerExpenseString = amountField.value;
+    const newPlayerExpense = parseInt(newPlayerExpenseString);
+    console.log(typeof newPlayerExpense)
+
+    const playerAmoutTotalElement = document.getElementById('player-amount');
+    /* const previousPlayerAmountTotalString = playerAmoutTotalElement.innerText;
+    const previousPlayerAmountTotal = parseInt(previousPlayerAmountTotalString);
+    console.log(typeof previousPlayerAmountTotal) */
+
+    const currentAmount = 5 * newPlayerExpense;
+    playerAmoutTotalElement.innerText = currentAmount;
+    amountField.value = '';
+
+
+})
 const playerList = [];
 function display(playerSlist) {
     console.log(playerSlist);
     const tableBody = document.getElementById('cart-player');
+
     tableBody.innerHTML = "";
-    for (let i = 0; i < playerSlist.length; i++) {
-        console.log(playerList[i].playerName);
-        const name = playerList[i].playerName;
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
+
+    for (let i = 1; i < playerSlist.length; i++) {
+        if (playerSlist.length <= 5) {
+            console.log(playerList[i].playerName);
+            const name = playerList[i].playerName;
+            const tr = document.createElement("tr");
+            tr.innerHTML = `
         <th>${i + 1}</th>
         <td>${name}</td>`;
-        tableBody.appendChild(tr);
+            tableBody.appendChild(tr);
+
+
+        }
+        else {
+            alert('Please try again');
+            return;
+
+
+
+        }
     }
 }
 function addToSelectedFive(element) {
@@ -27,4 +59,17 @@ function addToSelectedFive(element) {
      console.log(playerList.length) */
     document.getElementById('selected-player').innerText = playerList.length;
     display(playerList);
+    disablE();
+
+
+
+
 }
+/* function disablE() {
+    document.getElementById('btn-disable').disabled = true;
+
+} */
+
+
+
+
