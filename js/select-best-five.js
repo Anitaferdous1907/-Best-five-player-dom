f0ba6c127d415d5a52989219946de439c18a3184
Anitaferdous1907 <anitaferdous009@gmail.com>
2022-08-23 17:57:15 +0600

    select player option

--- js/select-best-five.js
+++ js/select-best-five.js
@@ -1,3 +1,30 @@
-function addToSelectedFive() {
-    console.log('clicked button')
+const playerList = [];
+function display(playerSlist) {
+    console.log(playerSlist);
+    const tableBody = document.getElementById('cart-player');
+    tableBody.innerHTML = "";
+    for (let i = 0; i < playerSlist.length; i++) {
+        console.log(playerList[i].playerName);
+        const name = playerList[i].playerName;
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+        <th>${i + 1}</th>
+        <td>${name}</td>`;
+        tableBody.appendChild(tr);
+    }
+}
+function addToSelectedFive(element) {
+    /*  console.log(element.parentNode.parentNode.children) */
+    /* console.log(element.parentNode.parentNode.children[1].innerText) */
+    const playerName = element.parentNode.parentNode.children[1].innerText;
+    console.log(playerName)
+    const playerobj = {
+        playerName: playerName
+
+    }
+    playerList.push(playerobj);
+    /*  console.log(playerList)
+     console.log(playerList.length) */
+    document.getElementById('selected-player').innerText = playerList.length;
+    display(playerList);
 }
